0f04176b93ef7a19863a816dfb90562bf6c19c3d
joroinnoroff <oinojorgen@gmail.com>
2025-10-07 23:02:45 +0200

    baseurl

--- src/api/records/route.ts
+++ src/api/records/route.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://brokken-back.vercel.app/api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export async function fetchRecords() {
   const res = await fetch(`${API_URL}/records`);
