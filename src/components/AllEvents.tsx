b3b3a87b8a6be2745fc007eb10acb73128f26f6b
joroinnoroff <oinojorgen@gmail.com>
2025-10-13 03:06:00 +0200

    removed unusedimp

--- src/components/AllEvents.tsx
+++ src/components/AllEvents.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { EventType, fetchEvents } from '@/lib/api';
-import e from 'express';
+
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
